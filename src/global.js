596ad1e618f46cd7d0c46c32cb553a0968f653a9
DannySuarez <suarezd10@gmail.com>
2020-03-22 17:11:25 -0700

    update global styles

--- src/global.js
+++ src/global.js
@@ -18,7 +18,14 @@ export const GlobalStyles = createGlobalStyle`
 		margin-bottom: 25px;
     background-color: #fdb641;
 		text-rendering: optimizeLegibility;
-		overflow: hidden;
+	}
+
+	main {
+		display: grid;
+		grid-gap: 3rem;
+		max-width: 1900px;
+		padding: 0 1.5rem;
+		margin: 2rem auto
 	}
 
 `;
