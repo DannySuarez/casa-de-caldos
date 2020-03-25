87bc8138f3019099e70f6b0484f90dd574925ba8
DannySuarez <suarezd10@gmail.com>
2020-03-25 11:44:46 -0700

    render Menu component

--- src/components/CasaDeCaldos.js
+++ src/components/CasaDeCaldos.js
@@ -3,12 +3,14 @@ import 'normalize.css';
 
 import { Header } from './header/Header';
 import { CustomerInfo } from './orderForms/CustomerInfo';
+import { Menu } from './menu/Menu';
 
 export default function CasaDeCaldos() {
   return (
     <>
       <Header />
       <CustomerInfo />
+      <Menu />
     </>
   );
 }
