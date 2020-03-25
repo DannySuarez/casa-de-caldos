1f7630850920c31b4dde738b7b38aca2e0a4705e
DannySuarez <suarezd10@gmail.com>
2020-03-25 11:44:02 -0700

    add menu component & mock menu + assets

---
+++ src/menu.js
@@ -0,0 +1,23 @@
+import tostada from './assets/tostada.jpg';
+import torta from './assets/torta.jpg';
+
+export const menuItems = [
+  {
+    name: 'Tostadas',
+    price: 5.99,
+    img: tostada,
+    description: 'Topped with beans, rice and lettuce' 
+  },
+  {
+    name: 'Torta',
+    price: 4.99,
+    img: torta, 
+    description: 'Stuffed with beans, rice and lettuce' 
+  },
+  {
+    name: 'Torta',
+    price: 4.99,
+    img: torta, 
+    description: 'Stuffed with beans, rice and lettuce' 
+  },
+];
