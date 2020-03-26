f409626fe52b6e980b4c4f998e3e50ccc45ab34c
DannySuarez <suarezd10@gmail.com>
2020-03-26 16:53:43 -0700

    update menu items

--- src/menu.js
+++ src/menu.js
@@ -3,18 +3,21 @@ import torta from './assets/torta.jpg';
 
 export const menuItems = [
   {
+    id: 1,
     name: 'Tostadas',
     price: 5.99,
     img: tostada,
     description: 'Topped with beans, rice and lettuce' 
   },
   {
+    id: 2,
     name: 'Torta',
     price: 4.99,
     img: torta, 
     description: 'Stuffed with beans, rice and lettuce' 
   },
   {
+    id: 3,
     name: 'Torta',
     price: 4.99,
     img: torta, 
