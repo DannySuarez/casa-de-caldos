74e807d993fa75fcd7ab99e4b24908b4afe41452
DannySuarez <suarezd10@gmail.com>
2020-03-29 00:06:16 -0700

    add assets

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import enchilada from './enchilada.png';
+import huarache from './huarache.png';
+import tamale from './tamales.png';
+import molePollo from './mole_pollo.png';
+
+export {
+  enchilada, huarache, tamale, molePollo
+};
