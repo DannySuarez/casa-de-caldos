c2778c80c9641ae1357565d9f3f70a5f06a077cf
DannySuarez <suarezd10@gmail.com>
2020-04-02 13:09:18 -0700

    add modal to MenuItem

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { MenuItemModal } from '../modals/MenuItemModal';
 import { Section } from './Menu.styled';
 
 export const MenuItem = ({ item }) => {
+  const [isOpen, setIsOpen] = useState(false);
+    
   return (
-    <Section className="section media cell--1-2" key={item.id}>
-      <div className="menu_item">
-        <div className="menu_image">
-          <img className="product" src={item.img} alt="Tostada" />
+    <>
+      <Section className="section media cell--1-2" key={item.id}>
+        <div className="menu_item">
+          <div className="menu_image">
+            <img className="product" src={item.img} alt={item.alt} />
+          </div>
+          <button onClick={() => setIsOpen(!isOpen)}>Add To Order</button>
+          <p className="product_price">${item.price.toFixed(2)}</p>
+          <h3>{item.name}</h3>
+          <p className="product_description">{item.smallDescription}</p>
         </div>
-        <button type="submit">Add To Order</button>
-        <p className="product_price">${item.price}</p>
-        <h3>{item.name}</h3>
-        <p className="product_description">{item.description}</p>
-      </div>
-    </Section>
+      </Section>
+
+      {isOpen && <MenuItemModal item={item} setIsOpen={setIsOpen}/>}
+
+    </>
   );
 };
 
