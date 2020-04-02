ec7a35cf7f7de22f53d8f6741ca0b2390e8f8756
DannySuarez <suarezd10@gmail.com>
2020-04-02 11:56:51 -0700

    clean up

--- src/components/CasaDeCaldos.js
+++ src/components/CasaDeCaldos.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'normalize.css';
 
 import { Header } from './header/Header';
-import { CustomerInfo } from './orderForms/CustomerInfo';
+import { CustomerInfo } from './customerForms/CustomerInfo';
 import { Menu } from './menu/Menu';
 
 export default function CasaDeCaldos() {
