b8b5a28e8cdc5efe7467ceb7171ed1eb8ff0b8ef
DannySuarez <suarezd10@gmail.com>
2020-04-04 18:43:54 -0700

    wrap app with provider

--- src/components/CasaDeCaldos.js
+++ src/components/CasaDeCaldos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'normalize.css';
+import { StoreProvider } from '../store/index';
 
 import { Header } from './header/Header';
 import { CustomerInfo } from './customerForms/CustomerInfo';
@@ -7,10 +8,10 @@ import { Menu } from './menu/Menu';
 
 export default function CasaDeCaldos() {
   return (
-    <>
+    <StoreProvider>
       <Header />
       <CustomerInfo />
       <Menu />
-    </>
+    </StoreProvider>
   );
 }
