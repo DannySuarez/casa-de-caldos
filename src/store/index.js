782a58fd72fdac225d0210b23849bd84f9af7783
DannySuarez <suarezd10@gmail.com>
2020-04-04 18:37:05 -0700

    create global state with combined reducers

---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
+import useCombinedReducers from 'use-combined-reducers';
+import customerReducer from '../reducers/customerReducer';
+import ordersReducer from '../reducers/ordersReducer';
+
+const customerInfoState = {
+  name: '',
+  email: '',
+  phone: ''
+};
+
+const ordersState = [];
+
+export const StateContext = createContext(null);
+
+export const StoreProvider = ({ children }) => {
+  
+  return (
+    <StateContext.Provider
+      value={useCombinedReducers({
+        customerInfo: useReducer(customerReducer, customerInfoState),
+        orders: useReducer(ordersReducer, ordersState)
+      })}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStore = () => useContext(StateContext);
+
+StoreProvider.propTypes = {
+  children: PropTypes.array
+};
