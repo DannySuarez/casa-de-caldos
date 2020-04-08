e45e88f652094b3181d395fea762be1adb3cc776
DannySuarez <suarezd10@gmail.com>
2020-04-08 12:30:08 -0700

    add context provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { GlobalStyles } from '../global';
-
+import { StoreProvider } from '../store';
 import CasaDeCaldos from './CasaDeCaldos';
 
 export default function App() {
   return (
-    <>
+    <StoreProvider>
       <GlobalStyles />
       <main>
         <CasaDeCaldos />
       </main>
-    </>
+    </StoreProvider>
   );
 }
