58e625f117f975974c9067ddd1b14647152dc6c0
DannySuarez <suarezd10@gmail.com>
2020-04-11 15:35:44 -0700

    update global styles

--- src/global.js
+++ src/global.js
@@ -37,4 +37,16 @@ export const GlobalStyles = createGlobalStyle`
 		border: none;
 	}
 
+	a {
+		text-decoration: none;
+	}
+
+	h1,h2,h3,h4,h5,h6 {
+		margin: 0;
+	}
+
+	h6 {
+		font-size: 14px;
+	}
+
 `;
