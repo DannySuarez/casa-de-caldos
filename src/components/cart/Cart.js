97128fa1fb8fd091cc7a8eddfa87cbaf4e94d1e2
DannySuarez <suarezd10@gmail.com>
2020-04-11 17:01:38 -0700

    add state and modal

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useStore } from '../../store';
 import { priceAdder } from '../../utils';
+import { FullCart } from '../modals/fullCart/FullCart';
+import { Modal } from '../modals/Modal';
 
 export const Cart = () => {
   const [{ orders }] = useStore();
-
+  const [isShowing, setIsShowing] = useState(false);
   const cartTotal = priceAdder(orders);
-  
+
+  const onClick = () => {
+    setIsShowing(isShowing => !isShowing);
+  };
+
   return (
     <ButtonWrapper>
-      <Button>
+      <Button onClick={onClick}>
         <span>{orders.length}</span>
-        <span>View Cart</span>${cartTotal}
+        <span>View Cart</span>${cartTotal.toFixed(2)}
       </Button>
+      {isShowing ? (<Modal><FullCart onClick={onClick} /></Modal>) : null}
     </ButtonWrapper>
   );
 };
