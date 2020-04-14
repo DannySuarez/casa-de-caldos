e2fbb0f5bea8d71f548bdf244c766137dc0f9670
DannySuarez <suarezd10@gmail.com>
2020-04-14 15:56:50 -0700

    remove scroll to top effect

--- src/hooks/useLockBodyScroll.js
+++ src/hooks/useLockBodyScroll.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 
 export const useLockBodyScroll = () => {
   useEffect(() => {
-    window.scrollTo(0, 0);
     const originalStyle = window.getComputedStyle(document.body).overflow;
     document.body.style.overflow = 'hidden';
 
