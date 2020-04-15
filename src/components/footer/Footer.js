850c1cdeca03e3c852ae16fa232ba92a0dba6e6d
DannySuarez <suarezd10@gmail.com>
2020-04-15 14:11:36 -0700

    feat: add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FooterWrapper } from './Footer.styled';
+
+export const Footer = () => {
+  const DIRECTIONS = 'https://www.google.com/maps/dir/?api=1&destination=954+N+Pacific+Hwy,+Woodburn,+OR+97071&travelmode=driving';
+
+  return (
+    <FooterWrapper>
+      <div>
+        <a href={DIRECTIONS} target="_blank" rel="noopener noreferrer">Directions</a>
+        <span>|</span>
+        <a href="tel:5039020664">Phone</a>
+      </div>
+      <figure>
+        <img 
+          src="https://res.cloudinary.com/thecodehouse/image/upload/c_scale,q_80,w_98/v1586966337/casa-de-caldos/mainLogo_1_i169yo.png" />
+      </figure>
+    </FooterWrapper>
+      
+  );
+};
---
+++ src/components/footer/Footer.styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.footer`
+  padding: 30px 0;
+  border-top: 1px solid var(--rust);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--rust);
+
+  div {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+
+    a:first-child {
+      text-align:right;
+    }
+
+    a {
+      color: var(--rust);
+      flex: 1 0 auto;
+      font-size: 1.2em;
+    }  
+
+    span {
+      margin: 0 10px;
+    }  
+  }
+
+  figure {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 30px 0;
+  }
+`;
