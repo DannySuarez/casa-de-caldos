9300a26d454a0eeffb4efc51591cd95d425f1955
DannySuarez <suarezd10@gmail.com>
2020-05-12 14:33:27 -0700

    implement Private Route in the Router

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -6,14 +6,22 @@ import {
 import CasaDeCaldos from '../CasaDeCaldos';
 import { Checkout } from '../checkout/Checkout';
 import { Thanks } from '../thanks/Thanks';
+import { CustomerOrders } from '../orders/CustomerOrders';
+import { SessionProvider } from '../auth/auth';
+import { SignIn } from '../login/SignIn';
+import PrivateRoute from './PrivateRoute';
 
 const Router = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={CasaDeCaldos} />
-      <Route path="/checkout" component={Checkout} />
-      <Route path="/thanks" component={Thanks} />
-    </Switch>
+    <SessionProvider>
+      <Switch>
+        <Route exact path="/" component={CasaDeCaldos} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/thanks" component={Thanks} />
+        <Route path="/signin" component={SignIn} />
+        <PrivateRoute path="/vieworders" component={CustomerOrders} />
+      </Switch>
+    </SessionProvider>
   );
 };
 
