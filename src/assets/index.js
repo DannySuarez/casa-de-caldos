52aa290768f4ca5deab5e428da5e4d3262c4afb5
DannySuarez <suarezd10@gmail.com>
2020-08-25 15:37:27 -0700

    update images

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,18 @@ import enchilada from './enchilada.png';
 import huarache from './huarache.png';
 import tamale from './tamales.png';
 import molePollo from './mole_pollo.png';
+import burrito from './burrito.jpg';
+import picadita from './picadita.jpg';
+import quesadilla from './quesadilla.jpeg';
+import taco from './taco.jpeg';
 
 export {
-  enchilada, huarache, tamale, molePollo
+  enchilada,
+  huarache,
+  tamale,
+  molePollo,
+  burrito,
+  picadita,
+  quesadilla,
+  taco,
 };
