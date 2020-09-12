4a9baf98ef37e74a88703587f1f14bda4075c53b
DannySuarez <suarezd10@gmail.com>
2020-09-12 15:18:01 -0700

    add form input component

--- src/components/customerForms/CustomerInfo.js
+++ src/components/customerForms/CustomerInfo.js
@@ -1,73 +1,69 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Section } from './CustomerInfo.styled';
-import { useCustomerSetStateContext, useCustomerStateContext } from '../context/CustomerInfoContext';
+import { Card } from './CustomerInfo.styled';
+import {
+  useCustomerSetStateContext,
+  useCustomerStateContext,
+} from '../context/CustomerInfoContext';
+import { FormInput } from './FormInput';
 
 export const CustomerInfo = ({ setIsActive }) => {
   const [isDisabled, setIsDisabled] = useState(false);
-  const { setName, setEmail, setPhone }  = useCustomerSetStateContext();
+  const { setName, setEmail, setPhone } = useCustomerSetStateContext();
   const { name, email, phone } = useCustomerStateContext();
 
   const onEnter = (e) => {
-    e.preventDefault();    
-    setIsDisabled(prevState => !prevState);
-    setIsActive(prevState => !prevState);
+    e.preventDefault();
+    setIsDisabled((prevState) => !prevState);
+    setIsActive((prevState) => !prevState);
   };
-  
+
   return (
-    <>
-      <Card>
-        <h2>Your Information</h2>
-        <p>
-          <strong>
-          No need to sign up for an account!
-          Just fill out some basic info in case we need to contact you.
-          </strong>
-        </p>
-        <form onSubmit={onEnter}>
-          <Section>
-            <label htmlFor="Full Name"><i>*</i>Full Name:</label>
-            <input
-              value={name}
-              onChange={e => setName(e.target.value)}
-              type="text"
-              id="Full Name"
-              name="Full Name"
-              disabled={isDisabled}
-              required
-            />
-          </Section>
-          <Section>
-            <label htmlFor="Email">Email Address:</label>
-            <input
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              type="text" 
-              id="Email"
-              name="Email"
-              disabled={isDisabled}
-            />
-          </Section>
-          <Section>
-            <label htmlFor="Phone"><i>*</i>Phone Number:</label>
-            <input
-              value={phone}
-              onChange={e => setPhone(e.target.value)}
-              type="tel"
-              id="Phone"
-              name="Phone"
-              placeholder="Phone"
-              disabled={isDisabled}
-              required
-            />
-          </Section>
-          <button>{ isDisabled ? 'Edit' : 'Continue' }</button>
-        </form>
-      </Card>
-    </>
+    <Card>
+      <h2>Your Information</h2>
+      <p>
+        <strong>
+            No need to sign up for an account! Just fill out some basic info in
+            case we need to contact you.
+        </strong>
+      </p>
+      <form onSubmit={onEnter}>
+        <FormInput
+          label="Full Name"
+          id="Full Name"
+          name="Full Name"
+          type="text"
+          required={true}
+          value={name}
+          disabled={isDisabled}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <FormInput
+          label="Email"
+          id="Email"
+          name="Email"
+          type="text"
+          required={false}
+          value={email}
+          disabled={isDisabled}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <FormInput
+          label="Phone"
+          id="Phone"
+          name="Phone"
+          type="tel"
+          required={true}
+          value={phone}
+          disabled={isDisabled}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <button>{isDisabled ? 'Edit' : 'Continue'}</button>
+      </form>
+    </Card>
   );
 };
 
 CustomerInfo.propTypes = {
-  setIsActive: PropTypes.func.isRequired
+  setIsActive: PropTypes.func.isRequired,
 };
